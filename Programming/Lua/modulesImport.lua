121218a277579ccb6a153addf30d79dc730bcf8c
xwang <xion36325@gmail.com>
2025-06-14 15:01:50 +0530

    update lua docs

--- Programming/Lua/modulesImport.lua
+++ Programming/Lua/modulesImport.lua
@@ -1,2 +1,6 @@
 local foo = require('Programming/Lua/modulesInLua')
 foo.coolF()
+
+local code = "return 2 + 3"
+local f = load(code)   -- compiles the string into a function
+print(f())             -- runs the compiled function => prints 5
--- Programming/Lua/modulesInLua.lua
+++ Programming/Lua/modulesInLua.lua
@@ -6,6 +6,44 @@ function M.coolF()
     print("Cool function called!")
 end
 print('Hello from modules `modules.lua`')
+
+function k()
+    print('Hello from `')
+end
 return M
 
-                                                                
+-- now to import `M` in another file, you can use:
+
+-- require, value will be cached so file run at most once                                                                 
+
+-- dofile, value not cached, so file run again if dofile is called again
+
+
+-- loadfile ->  takes a filename and Compiles Lua code from file, and reads the file contents, returns a function in which the compiled code is stored
+
+
+
+    
+-- load() 
+-- load() takes Lua code (as a string or function) and compiles it into a Lua function, without running it.
+-- You can run that compiled function later.
+-- It's basically like creating Lua code dynamically at runtime.
+
+-- Example: 
+--[[ 
+
+local code = "return 2 + 3"
+local f = load(code)   -- compiles the string into a function
+-- behind the scences this happens: 
+        ``` 
+        local f = function()
+            return 2 + 3
+        end
+        ```
+- so the code you give in load, enclosed inside a funtion and return by load         
+
+print(f())             -- runs the compiled function => prints 5
+
+     ]] 
+    -- load() returns nil and error message if there was a syntax error.
+

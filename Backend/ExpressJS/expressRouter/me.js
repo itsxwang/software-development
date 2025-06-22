28ea16a477a278996076d0ba123fc0679caa1fdc
xwang <xion36325@gmail.com>
2025-06-23 00:45:39 +0530

    make very basic project in express for practice

--- Backend/ExpressJS/expressRouter/me.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-console.log(path.join(__dirname, "views", "404.html"));
-
-
-console.log(path.dirname(require.main.filename));
---
+++ Backend/ExpressJS/expressRouter/routes/home.js
@@ -0,0 +1,25 @@
+
+// core
+const path = require("path");
+
+// external
+const express = require("express");
+
+// local 
+const pathUtil = require("../utils/pathUtil");
+
+
+const router = express.Router();
+
+
+router.get("/home", (req, res) =>
+  res.sendFile(path.join(pathUtil, "views", "home.html"))
+);
+
+router.get("/", (req, res) =>
+  res.sendFile(path.join(pathUtil, "views", "home.html"))
+);
+
+
+module.exports = router;
+

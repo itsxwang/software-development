1d63fbfc9b3c7e7784cc5612a658394d76a1c717
xwang <xion36325@gmail.com>
2025-10-02 01:51:53 +0530

    add blockchain related concepts

--- Programming/Rust/chapter1/src/main.rs
+++ Programming/Rust/chapter1/src/main.rs
@@ -1,6 +1,30 @@
 // a greate system programming language
 // rust becomes memory safe because of borrower, ownership and lifetimes
 
+use std::string;
+
+#[allow(unused_variables)]
+/* In Rust, the #[ ... ] syntax is called an attribute.
+
+They are pieces of metadata that you can attach to modules, crates, functions, structs, enums, variables, etc. to control how the compiler treats them.
+
+General forms of attributes
+
+    - Outer attributes: #[ ... ] → apply to the item that comes after them.
+    - Inner attributes: #![ ... ] → apply to the item they are inside.
+
+
+    => Common built-in attributes
+
+       -  #[derive(...)] → automatically implement traits like Debug, Clone, etc.
+
+       - #[allow(...)], #[warn(...)], #[deny(...)] → control compiler warnings.
+
+       - #[test] → marks a function as a test.
+
+       - #[cfg(...)] → conditional compilation (e.g., only compile on Windows).
+*/
+
 fn main() {
     println!("Hello, world!");
     // variables in rust: https://youtu.be/qP7LzZqGh30?si=l5DAixTVOLDSjIRj&t=2227
@@ -9,7 +33,35 @@ fn main() {
     println!("{}", x);
     // by default variable naturally are immutable
     // constants are immutable
-    
+    let str = "hello";
+    println!("{}", str);
+
+    // destructure tuple
+    let tup = (1, 2, 3);
+    let (mut a, b, c) = tup;
+    a += 6;
+    println!("{} {} {}", a, b, c);
+
+    // .. means everything else
+    let (x, ..) = (7, 5, 4);
+    // same way can be used to destructure arrays
+
+    // we can anootate type on a value too
+    println!("{}", 1_i8);
+
+    // In Rust, the `as` keyword is mainly used for explicit type conversion (casting), and sometimes for renaming imports.
+    let x: f64 = 1.10;
+    let y = x as i32;
+    print!("{}", myfunc());
 }
 
-// btw very cool definition of funcrtion: function is a named block of code that is reusable 
+std::any::type_name
+// other funcs
+
+fn myfunc() -> i32
+ {
+    "hello".to_string();
+    return 0;
+ }
+
+// btw very cool definition of funcrtion: function is a named block of code that is reusable
---
+++ web3/btcExplained.md
@@ -0,0 +1,20 @@
+- [BTC white paper demestify](https://youtu.be/NoqNhWnjE1Q?si=5Z6tv8S7pAjAAUdz=7)
+
+---
+
+- TimeStamp Server: In blockchain, each block contains a timestamp(this includes the date, time, and timezone) and a link to the previous block, forming a chain. This ensures that the data in the 
+blocks cannot be altered without changing all subsequent blocks, providing security and integrity to the blockchain.
+\
+**Timestamp server** is a way to prove that a piece of data existed at a certain point in time. It works by taking a hash of the data and publishing it in a public place, such as a newspaper or a website. This creates a permanent record that can be used to verify the existence of the data at that time.
+
+
+- **Proof of Work**: Proof of Work (PoW) is a consensus mechanism used in blockchain networks to validate transactions and create new blocks. It requires participants (miners) to solve complex mathematical puzzles, which requires significant computational power and energy. The first miner to solve the puzzle gets to add the new block to the blockchain and is rewarded with cryptocurrency. This process ensures the security and integrity of the blockchain by making it difficult and costly to alter past transactions.
+
+- [Network section in paper](https://youtu.be/NoqNhWnjE1Q?si=c1cQ9x-DZN5FzcgN&t=77)
+
+- [Incentivize section](https://youtu.be/NoqNhWnjE1Q?si=BQ9Hb9rioqEA4WXm&t=97)
+
+- [Reclaiming disk space section](https://youtu.be/NoqNhWnjE1Q?si=mOfWRjsoABpmPS38&t=107)
+- [Simpilified payment verification section](https://youtu.be/NoqNhWnjE1Q?si=mOfWRjsoABpmPS38&t=107), basically allows you to run very lightweight node without much memory
+
+- [Combining and splitting value section](https://youtu.be/NoqNhWnjE1Q?si=5uAt6xaux5wwTFzd&t=147), in this section it explains how transactions can have multiple inputs and outputs, allowing for more complex transactions and better privacy.

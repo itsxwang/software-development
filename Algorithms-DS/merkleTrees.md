f06758fb4eced743072e7f5f46577708f3d11fe1
xwang <xion36325@gmail.com>
2025-10-05 19:05:36 +0530

    merkle trees explain

---
+++ Algorithms-DS/merkleTrees.md
@@ -0,0 +1,5 @@
+- [Merkle trees applications](https://youtu.be/qHMLy5JjbjQ?si=8maafBTQg-ZQ8RFS&t=7)
+
+- [How it works using git filesystem](https://youtu.be/qHMLy5JjbjQ?si=DV9o6oc0wdYPfsfQ&t=27)
+
+- [Merkle tree example with blockchain](https://youtu.be/fB41w3JcR7U?si=icIDP-Lhk4IpZIR5&t=7)
--- Programming/Rust/chapter1/src/main.rs
+++ Programming/Rust/chapter1/src/main.rs
@@ -1,7 +1,7 @@
 // a greate system programming language
 // rust becomes memory safe because of borrower, ownership and lifetimes
 
-use std::string;
+use std::io::Read;
 
 #[allow(unused_variables)]
 /* In Rust, the #[ ... ] syntax is called an attribute.
@@ -30,7 +30,7 @@ fn main() {
     // variables in rust: https://youtu.be/qP7LzZqGh30?si=l5DAixTVOLDSjIRj&t=2227
     let x: i8 = 7; // by default i32
     assert_eq!(x, 7); // if not equal, panic(means immediately terminate the program)
-    println!("{}", x);
+    println!("{}", x); // println! is a macro as it called with `!`,  but println is a function
     // by default variable naturally are immutable
     // constants are immutable
     let str = "hello";
@@ -44,6 +44,10 @@ fn main() {
 
     // .. means everything else
     let (x, ..) = (7, 5, 4);
+
+    // to print the collection of values
+    // println!("{:?}", (1..2)); {:?} is for debugging means print the value
+
     // same way can be used to destructure arrays
 
     // we can anootate type on a value too
@@ -52,16 +56,27 @@ fn main() {
     // In Rust, the `as` keyword is mainly used for explicit type conversion (casting), and sometimes for renaming imports.
     let x: f64 = 1.10;
     let y = x as i32;
-    print!("{}", myfunc());
+    println!("{}", myfunc(y));
+
+    // loop_func('f');
+
+    // type conversion
+    let parsed_int = "7".parse::<i32>().expect("failed to parse");
+    println!("{}", parsed_int);
+
+    // to int from char
 }
 
-std::any::type_name
-// other funcs
-
-fn myfunc() -> i32
- {
+fn myfunc(x: i32) -> i32 {
     "hello".to_string();
-    return 0;
- }
+    return x;
+}
+
+// fn loop_func(x: char) {
+//    // range and range inclusive: https://youtu.be/BpPEoZW5IiY?si=d_eAISLnrRbELfy-&t=3487
+//     for i in 'a'..x {
+//         println!("{}", i);
+//     }
+// }
 
 // btw very cool definition of funcrtion: function is a named block of code that is reusable

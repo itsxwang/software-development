e84ce1db7dbb2f92220b253883ab9c089e1711c8
xwang <xion36325@gmail.com>
2026-01-24 15:22:08 +0530

    update rust repo

---
+++ Programming/Rust/chapter3/src/bin/febannoci.rs
@@ -0,0 +1,102 @@
+use std::io::{self, Write}; 
+// Rust’s grouped (nested) import syntax
+/* 
+use path::to::thing;
+Means:
+
+“Bring thing into the current scope under this name”
+
+- It does not copy code
+- It does not execute code
+- It only affects names
+
+use std::io::{self, Write};
+This is just short syntax for multiple imports.
+
+The line above is exactly the same as writing:
+
+use std::io;
+use std::io::Write;
+*That’s it. Nothing more.*
+
+self
+Means:
+“Import the module itself”
+
+`Write`
+Means:
+“Import the Write item from std::io”
+Write is a trait.
+
+-> so use std::io::{self, Write};
+    brings std::io into scope as io
+    If you removed self, then io:: would NOT exist.
+
+    Question:
+    Why doesn’t this compile without Write?
+    
+    io::stdout().flush();
+    Answer:
+    Because flush() is not a method on stdout itself.
+    
+    It comes from the Write trait.
+    
+    Rust rule:
+    
+    Methods from traits are only available if the trait is in scope
+
+
+    So:
+        `use std::io::Write;`
+        tells the compiler:
+        “Yes, I want to use methods from this trait.”
+
+        Visual mental model (remember this)
+                std
+                └── io (module)
+                  ├── stdin()   (function)
+                  ├── stdout()  (function)
+                  └── Write     (trait)
+                       └── flush() (method)
+        
+*/
+
+
+fn main() {
+    // 1 1 2 3 5 8 13...
+    'mainl: loop {
+        let mut a = 1;
+        let mut b = 1;
+        print!("Write number: ");
+        io::stdout().flush().unwrap(); // 🔥 IMPORTANT: “Send everything in the output buffer to the terminal right now” 
+        /*         
+            Without it:
+        text stays in memory
+        terminal shows it late
+         */
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read!");
+        let user_in: i32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter only valid number!");
+                continue;
+            }
+        };
+        if user_in == 0 {
+            println!("{} {}", a, b);
+            break;
+        };
+        println!("-----------------------------");
+        loop {
+            if b > user_in {
+                break 'mainl;
+            }
+            println!("{}", b);
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
--- Programming/Rust/chapter3/src/main.rs
+++ Programming/Rust/chapter3/src/main.rs
@@ -1,15 +1,52 @@
+fn five() -> i32 {
+    5
+}
+
 fn main() {
     println!("Hello, world!");
     const THREE_HOURS_IN_SECONDS: i32 = 60 * 60 * 3;
     println!("Three hours in seconds is: {}", THREE_HOURS_IN_SECONDS);
-    
+
     // tuples can't be changed but their elements can be accessed individually (x,y,z) = tup
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     // let (x, y, z) = tup;
-    // access by indexing, `tup.0`, `tup.n`  . 
+    // access by indexing, `tup.0`, `tup.n`  .
     println!("x {}", tup.0);
-    
+
     let ra: [i32; 5] = [1, 2, 3, 4, 5];
     println!("ra {:?}", ra);
+    let xx = five();
+    println!("xx {}", xx);
 
+    let x = if true {
+        let expresso = String::from("red");
+        expresso.len()
+    } else {
+        0
+    };
+    println!("x {}", x);
+
+    // loop {} -> continue and break supported
+
+    let mut counter = 0;
+    let below10 = loop {
+        if counter == 9 {
+            break counter;
+        } else {
+            counter += 1
+        }
+    };
+    println!("below10 {}", below10);
+
+    // 'looplabels: loop {}
+    // let a = [10, 20, 30, 40, 50];
+
+    // for element in a {
+    //     println!("the value is: {element}");
+    // }
+
+    // machine code produce from for loops are much efficient because: 1- Compiler don't have to add a runtime code for make a checks to compare a index value with array len at every iteration
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
 }

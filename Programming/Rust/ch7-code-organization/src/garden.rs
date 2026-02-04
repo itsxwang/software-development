3891f8b9bed77ca9f069a43ccdff03e654af86d5
xwang <xion36325@gmail.com>
2026-02-05 01:39:03 +0530

    rust: packages, modules, crates & imports

---
+++ Programming/Rust/ch7-code-organization/src/garden.rs
@@ -0,0 +1,3 @@
+pub mod veg;
+pub struct Garden {}
+
--- Programming/Rust/ch7-code-organization/src/main.rs
+++ Programming/Rust/ch7-code-organization/src/main.rs
@@ -1,5 +1,12 @@
+use garden::veg::Asparagus;
+
+pub mod garden;
+
 fn main() {
+    //
+    Asparagus {};
     println!("Hello, world!");
-    // https://doc.rust-lang.org/book/ch07-01-packages-and-crates.html#packages-and-crates -> 
-    
+    // https://doc.rust-lang.org/book/ch07-01-packages-and-crates.html#packages-and-crates ->
+
+    // https://doc.rust-lang.org/book/ch07-02-defining-modules-to-control-scope-and-privacy.html#modules-cheat-sheet
 }

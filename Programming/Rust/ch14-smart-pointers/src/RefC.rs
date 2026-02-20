1c8d20e2e87cf0e9ae0c7970e772bfc2767e191c
xwang <xion36325@gmail.com>
2026-02-20 19:39:46 +0530

    rs: Weak Counts

--- Programming/Rust/ch14-smart-pointers/src/RefC.rs
+++ Programming/Rust/ch14-smart-pointers/src/RefC.rs
@@ -1,4 +1,8 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+    sync::RwLockReadGuard,
+};
 
 pub fn ref_c() {
     // https://doc.rust-lang.org/book/ch15-05-interior-mutability.html#refcellt-and-the-interior-mutability-pattern
@@ -25,4 +29,40 @@ pub fn ref_c() {
     println!("{}", e);
 
     // https://doc.rust-lang.org/book/ch15-05-interior-mutability.html#allowing-multiple-owners-of-mutable-data
+
+    // ------------------------------------------------------------------------------
+
+    // https://doc.rust-lang.org/book/ch15-06-reference-cycles.html#reference-cycles-can-leak-memory
+    // https://doc.rust-lang.org/book/ch15-06-reference-cycles.html#creating-a-reference-cycle
+
+    // https://doc.rust-lang.org/book/ch15-06-reference-cycles.html#preventing-reference-cycles-using-weakt
+
+    #[derive(Debug)]
+    struct Tree {
+        value: i32,
+        parent: RefCell<Weak<Tree>>,
+        children: RefCell<Vec<Rc<Tree>>>,
+    }
+
+    let leaf = Rc::new(Tree {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    println!(" leaf parent:  {:?}", leaf.parent.borrow().upgrade());
+    let branch = Rc::new(Tree {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    println!("branch weak count: {:?}", Rc::weak_count(&branch));
+    leaf.parent.replace(Rc::downgrade(&branch));
+    println!("branch weak count: {:?}", Rc::weak_count(&branch));
+
+    println!("leaf parent: {:?}", leaf.parent.borrow().upgrade());
+
+    // https://doc.rust-lang.org/book/ch15-06-reference-cycles.html#preventing-reference-cycles-using-weakt
+
+    // https://doc.rust-lang.org/book/ch15-06-reference-cycles.html#visualizing-changes-to-strong_count-and-weak_count
 }

dd8a2a04b596db6bd82637da258cacaf890a2fe0
xwang <xion36325@gmail.com>
2026-03-05 00:46:02 +0530

    rs: webserver-multiThreaded

---
+++ Programming/Rust/webServer/src/lib.rs
@@ -0,0 +1,20 @@
+pub struct ThreadPool;
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+        ThreadPool
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        std::thread::spawn(f);
+    }
+}
